4bb2e228afaa4f5d4453452de0537f84b023606b
Mike Kaliman <kaliman.mike@gmail.com>
2020-10-28 17:57:47 -0400

    Add more guide links and re-arrange code

--- README.md
+++ README.md
@@ -6,12 +6,16 @@ FDQN: `arduino:samd:nano_33_iot`
 
 ### Soil moisture
 YL-69 soil moisture sensor to check plant health
+- Connect with D6 and A6
 [Quick Arduino write-up](https://create.arduino.cc/projecthub/nekhbet/using-the-yl-39-yl-69-soil-humidity-sensor-with-arduino-968268)
 
 ### Light sensor
+- Connect with I2C
 [BH1750](https://www.adafruit.com/product/4681)
 [Arduino C++ Library: "hp_BH1750"](https://github.com/Starmbi/hp_BH1750)
 
 ### Display
+- Connect with SPI
 [eInk display](https://www.adafruit.com/product/4086)
+[Pinout guide](https://learn.adafruit.com/adafruit-eink-display-breakouts/pinouts)
 [Arduino C++ Library: "Adafruit EPD"](https://github.com/adafruit/Adafruit_EPD)
--- plant-monitor.ino
+++ plant-monitor.ino
@@ -4,7 +4,7 @@
 #include <hp_BH1750.h>
 
 constexpr byte humidity_sensor_pin = A1;
-constexpr byte humidity_sensor_vcc = 6;
+constexpr byte humidity_sensor_vcc = 6; // D6
 constexpr int baud_rate = 9600;
 constexpr int second_ms = 1000;
 constexpr int ten_seconds_ms = 10 * second_ms;
@@ -19,21 +19,53 @@ constexpr auto EPD_BUSY = 7;
 Adafruit_IL0373 display(212, 104, EPD_DC, EPD_RESET, EPD_CS, SRAM_CS, EPD_BUSY);
 hp_BH1750 lightMeter;
 
-void setup() {
-  // Init the humidity sensor board
-  pinMode(humidity_sensor_vcc, OUTPUT);
-  digitalWrite(humidity_sensor_vcc, LOW);
-
-  Wire.begin(); // Init I2C
-
-  while (!Serial) {
-    // Wait for serial to be ready
+struct HumiditySensor {
+  static void setup() {
+    // Init the humidity sensor board
+    pinMode(humidity_sensor_vcc, OUTPUT);
+    digitalWrite(humidity_sensor_vcc, LOW);
   }
-  delay(1000);
-  Serial.begin(baud_rate);
-  const bool ready = lightMeter.begin(BH1750_TO_GROUND);
+  static int read() {
+    digitalWrite(humidity_sensor_vcc, HIGH);
+    delay(500);
+    const int value = analogRead(humidity_sensor_pin);
+    digitalWrite(humidity_sensor_vcc, LOW);
+    return 1023 - value;
+  }
+  static void write_output() {
+    Serial.print("Humidity: ");
+    const auto moisture_perc = get_moisture_percentage();
+    Serial.print(moisture_perc);
+    Serial.println("%");
+    // Have to wait 10 seconds per humidity measurement
+    delay(ten_seconds_ms);
+  }
+  // Soil moisture range
+  static constexpr int measured_moisture_min = 120; // Measured in air (dry)
+  static constexpr int measured_moisture_max = 510; // Measured in a glass of water
 
-  {
+  static int get_moisture_percentage() {
+    const auto unadjusted_range = HumiditySensor::read();
+    // Limit values between the measaured range just to be sure
+    const auto constrained_range =
+        constrain(unadjusted_range, measured_moisture_min, measured_moisture_max);
+    // Map the range to a percentage
+    const auto moisture_percentage =
+        map(constrained_range, measured_moisture_min, measured_moisture_max, 0, 100);
+
+    return moisture_percentage;
+  }
+};
+
+struct Display {
+  static void draw_text(const char* text, uint16_t color) {
+    display.setCursor(0, 0);
+    display.setTextColor(color);
+    display.setTextWrap(true);
+    display.print(text);
+  }
+
+  static void setup() {
     // Display "hello world" for 5 seconds
     display.begin();
     display.clearBuffer();
@@ -41,9 +73,7 @@ void setup() {
     display.display();
     delay(five_seconds_ms);
     display.clearBuffer();
-  }
 
-  {
     // Draw some lines
     for (int16_t i = 0; i < display.width(); i += 4) {
       display.drawLine(0, 0, i, display.height() - 1, EPD_BLACK);
@@ -54,54 +84,46 @@ void setup() {
     }
     display.display();
   }
-}
+};
 
-int read_humidity_sensor() {
-  digitalWrite(humidity_sensor_vcc, HIGH);
-  delay(500);
-  const int value = analogRead(humidity_sensor_pin);
-  digitalWrite(humidity_sensor_vcc, LOW);
-  return 1023 - value;
-}
+struct LightSensor {
+  static void setup() {
+    if (lightMeter.begin(BH1750_TO_GROUND)) {
+      lightMeter.calibrateTiming();
+    } else {
+      Serial.println("Light meter wasn't ready!");
+    }
+  }
 
-// Soil moisture range
-constexpr int measured_moisture_min = 120; // Measured in air (dry)
-constexpr int measured_moisture_max = 510; // Measured in a glass of water
+  static void maybe_write_output() {
+    if (lightMeter.hasValue()) {
+      const float lux = lightMeter.getLux();
+      Serial.print("Light: ");
+      Serial.print(lux);
+      Serial.println("lux");
+      lightMeter.start();
+    }
+  }
+};
 
-int get_moisture_percentage() {
-  const auto unadjusted_range = read_humidity_sensor();
-  // Limit values between the measaured range just to be sure
-  const auto constrained_range =
-      constrain(unadjusted_range, measured_moisture_min, measured_moisture_max);
-  // Map the range to a percentage
-  const auto moisture_percentage =
-      map(constrained_range, measured_moisture_min, measured_moisture_max, 0, 100);
+void setup() {
 
-  return moisture_percentage;
+  Wire.begin(); // Init I2C
+
+  while (!Serial) {
+    // Wait for serial to be ready
+  }
+  delay(1000);
+
+  Serial.begin(baud_rate);
+
+  HumiditySensor::setup();
+  LightSensor::setup();
+  Display::setup(); // Display is written to during setup
 }
 
 void loop() {
-  {
-    Serial.print("Humidity: ");
-    const auto moisture_perc = get_moisture_percentage();
-    Serial.print(moisture_perc);
-    Serial.println("%");
-    // Have to wait 10 seconds per humidity measurement
-    delay(ten_seconds_ms);
-  }
 
-  {
-    lightMeter.start();
-    const float lux = lightMeter.getLux();
-    Serial.print("Light: ");
-    Serial.print(lux);
-    Serial.println("lux");
-  }
-}
-
-void draw_text(const char* text, uint16_t color) {
-  display.setCursor(0, 0);
-  display.setTextColor(color);
-  display.setTextWrap(true);
-  display.print(text);
+  HumiditySensor::write_output();
+  LightSensor::maybe_write_output(); // Output may not always be available
 }
