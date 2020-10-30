f212240bfefd9cf6aa3d9a9f5378acb9a2a882ef
Mike Kaliman <kaliman.mike@gmail.com>
2020-10-30 11:05:05 -0400

    Sensors read a bit more asynchronously

---
+++ Optional.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+namespace mik {
+
+/**
+ *  @brief Super basic implementation of std::optional, limited by default Arduino toolchain
+ * settings
+ */
+template <class T>
+class Optional {
+private:
+  T value_;
+  bool has_value_;
+
+public:
+  constexpr Optional() noexcept : has_value_(false) {}
+
+  explicit Optional(const T& value) {
+    value_ = value;
+    has_value_ = true;
+  }
+
+  explicit Optional(T&& value) {
+    value_ = value;
+    has_value_ = true;
+  }
+
+  T& value() { return value_; }
+
+  constexpr bool has_value() const noexcept { return has_value_; }
+
+  constexpr T value_or(T&& other) const noexcept {
+    if (has_value_) {
+      return value_;
+    } else {
+      return other;
+    }
+  }
+};
+
+} // namespace mik
--- plant-monitor.ino
+++ plant-monitor.ino
@@ -1,14 +1,17 @@
+#include "Optional.hpp"
 #include <Adafruit_EPD.h>
 #include <Arduino.h>
 #include <Wire.h>
 #include <hp_BH1750.h>
+namespace mik {
 
 constexpr byte humidity_sensor_pin = A1;
 constexpr byte humidity_sensor_vcc = 6; // D6
 constexpr int baud_rate = 9600;
-constexpr int second_ms = 1000;
-constexpr int ten_seconds_ms = 10 * second_ms;
-constexpr int five_seconds_ms = 5 * second_ms;
+constexpr unsigned long second_ms = 1000;
+constexpr unsigned long minute_ms = 60 * second_ms;
+constexpr unsigned long ten_seconds_ms = 10 * second_ms;
+constexpr unsigned long five_seconds_ms = 5 * second_ms;
 
 constexpr auto EPD_CS = 10;
 constexpr auto EPD_DC = 9;
@@ -19,75 +22,65 @@ constexpr auto EPD_BUSY = 7;
 Adafruit_IL0373 display(212, 104, EPD_DC, EPD_RESET, EPD_CS, SRAM_CS, EPD_BUSY);
 hp_BH1750 lightMeter;
 
-struct HumiditySensor {
-  static void setup() {
+/**
+ * @brief Can only read from this sensor every 10 seconds
+ */
+class HumiditySensor {
+private:
+  static constexpr long humidity_read_interval_ms = 500;
+  /**
+   * @brief blocks for 500ms
+   */
+  uint32_t read_raw() {
+    digitalWrite(humidity_sensor_vcc, HIGH);
+    delay(humidity_read_interval_ms);
+    const uint32_t value = analogRead(humidity_sensor_pin);
+    digitalWrite(humidity_sensor_vcc, LOW);
+    return 1023U - value;
+  }
+
+  /// @brief Last time that the sensor was read using millis()
+  unsigned long sensor_read_timestamp_ms_;
+
+public:
+  void setup() {
     // Init the humidity sensor board
     pinMode(humidity_sensor_vcc, OUTPUT);
     digitalWrite(humidity_sensor_vcc, LOW);
-  }
-  static int read() {
-    digitalWrite(humidity_sensor_vcc, HIGH);
-    delay(500);
-    const int value = analogRead(humidity_sensor_pin);
-    digitalWrite(humidity_sensor_vcc, LOW);
-    return 1023 - value;
-  }
-  static void write_output() {
-    Serial.print("Humidity: ");
-    const auto moisture_perc = get_moisture_percentage();
-    Serial.print(moisture_perc);
-    Serial.println("%");
-    // Have to wait 10 seconds per humidity measurement
-    delay(ten_seconds_ms);
+    // Technically didn't read from it, but meh
+    sensor_read_timestamp_ms_ = 0;
   }
   // Soil moisture range
-  static constexpr int measured_moisture_min = 120; // Measured in air (dry)
-  static constexpr int measured_moisture_max = 510; // Measured in a glass of water
+  static constexpr uint32_t measured_moisture_min = 120; // Measured in air (dry)
+  static constexpr uint32_t measured_moisture_max = 510; // Measured in a glass of water
 
-  static int get_moisture_percentage() {
-    const auto unadjusted_range = HumiditySensor::read();
-    // Limit values between the measaured range just to be sure
-    const auto constrained_range =
-        constrain(unadjusted_range, measured_moisture_min, measured_moisture_max);
-    // Map the range to a percentage
-    const auto moisture_percentage =
-        map(constrained_range, measured_moisture_min, measured_moisture_max, 0, 100);
+  /**
+   * @brief Get moisture percentage if available
+   */
+  Optional<uint32_t> read() {
+    if (millis() - sensor_read_timestamp_ms_ < ten_seconds_ms) {
+      const auto unadjusted_range = read_raw();
+      // Limit values between the measaured range just to be sure
+      const auto constrained_range =
+          constrain(unadjusted_range, measured_moisture_min, measured_moisture_max);
+      // Map the range to a percentage
+      const auto moisture_percentage =
+          map(constrained_range, measured_moisture_min, measured_moisture_max, 0, 100);
 
-    return moisture_percentage;
+      return Optional<uint32_t>(moisture_percentage);
+    } else {
+      // Sensor was read too recently
+      return Optional<uint32_t>();
+    }
   }
 };
 
-struct Display {
-  static void draw_text(const char* text, uint16_t color) {
-    display.setCursor(0, 0);
-    display.setTextColor(color);
-    display.setTextWrap(true);
-    display.print(text);
-  }
-
-  static void setup() {
-    // Display "hello world" for 5 seconds
-    display.begin();
-    display.clearBuffer();
-    draw_text("Hello world", EPD_BLACK);
-    display.display();
-    delay(five_seconds_ms);
-    display.clearBuffer();
-
-    // Draw some lines
-    for (int16_t i = 0; i < display.width(); i += 4) {
-      display.drawLine(0, 0, i, display.height() - 1, EPD_BLACK);
-    }
-
-    for (int16_t i = 0; i < display.height(); i += 4) {
-      display.drawLine(display.width() - 1, 0, 0, i, EPD_RED); // on grayscale this will be mid-gray
-    }
-    display.display();
-  }
-};
-
-struct LightSensor {
-  static void setup() {
+/**
+ * @brief hp_BH1705 library already manages delay
+ */
+class LightSensor {
+public:
+  void setup() {
     if (lightMeter.begin(BH1750_TO_GROUND)) {
       lightMeter.calibrateTiming();
     } else {
@@ -95,35 +88,108 @@ struct LightSensor {
     }
   }
 
-  static void maybe_write_output() {
+  Optional<float> read() {
     if (lightMeter.hasValue()) {
-      const float lux = lightMeter.getLux();
-      Serial.print("Light: ");
-      Serial.print(lux);
-      Serial.println("lux");
-      lightMeter.start();
+      return Optional<float>(lightMeter.getLux());
+    } else {
+      return Optional<float>();
     }
   }
 };
 
+class Display {
+private:
+  static constexpr auto init_msg_display_x = 0;
+  static constexpr auto init_msg_display_y = 0;
+  static constexpr auto humidity_display_x = 0;
+  static constexpr auto humidity_display_y = 36;
+  static constexpr auto light_display_x = 0;
+  static constexpr auto light_display_y = 72;
+  static constexpr long display_interval_ms = 3 * minute_ms;
+  /// @brief Last time that the display was written to using millis()
+  unsigned long display_write_timestamp_ms_;
+
+public:
+  void setup() {
+    display.begin();
+    display.clearBuffer();
+
+    display.setCursor(init_msg_display_x, init_msg_display_y);
+    display.setTextColor(EPD_BLACK);
+    display.setTextWrap(false);
+    display.print("plantmon init");
+    display.display();
+
+    // Display text for 5 seconds
+    delay(five_seconds_ms);
+
+    display.clearBuffer();
+    display.display();
+    display_write_timestamp_ms_ = millis();
+  }
+
+  /**
+   * @brief Display gets updated here along with delaying for the recommended interval
+   */
+  void update(Optional<uint32_t> moisture_percentage, Optional<float> lux) {
+    bool wrote_to_display = false;
+
+    if (moisture_percentage.has_value()) {
+      display.setCursor(humidity_display_x, humidity_display_y);
+
+      const auto text = String("Moisture: " + String(moisture_percentage.value()) + "%");
+      display.print(text);
+      wrote_to_display = true;
+    }
+
+    if (lux.has_value()) {
+      display.setCursor(light_display_x, light_display_y);
+
+      const auto text = String("Light: " + String(lux.value()) + " lux");
+      display.print(text);
+      wrote_to_display = true;
+    }
+
+    // Able to write once again
+    if (millis() - display_write_timestamp_ms_ < display_interval_ms) {
+      display.display();
+      display_write_timestamp_ms_ = millis();
+    }
+  }
+};
+
+} // namespace mik
+
+/**
+ * -----------------------------------
+ * Main Arduino logic
+ * -----------------------------------
+ */
+mik::HumiditySensor humidity_sensor;
+mik::LightSensor light_sensor;
+mik::Display eink_display;
+
 void setup() {
+  using namespace mik;
 
   Wire.begin(); // Init I2C
 
   while (!Serial) {
     // Wait for serial to be ready
   }
-  delay(1000);
+  delay(second_ms);
 
   Serial.begin(baud_rate);
 
-  HumiditySensor::setup();
-  LightSensor::setup();
-  Display::setup(); // Display is written to during setup
+  humidity_sensor.setup();
+  light_sensor.setup();
+  eink_display.setup(); // Display is written to during setup
 }
 
 void loop() {
-
-  HumiditySensor::write_output();
-  LightSensor::maybe_write_output(); // Output may not always be available
+  using namespace mik;
+  // Output may not always be available from the sensor, but they will be read from if so
+  eink_display.update(humidity_sensor.read(), light_sensor.read());
+  // Just delay regardless to avoid using more power than needed
+  delay(five_seconds_ms);
 }
